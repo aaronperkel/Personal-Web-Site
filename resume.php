e84caee1659d07a62c45c2526261fc23e3a93525
aaronperkel <aaron.perkel27@gmail.com>
2024-07-25 17:09:52 -0400

    initial commit

---
+++ resume.php
@@ -0,0 +1,184 @@
+<?php include 'top.php'; ?>  
+        <main>
+            <div class="resumeGrid">
+                <div class="columnOne">
+                    <h1>Aaron Perkel</h1>
+                    <p class="orange">
+                        478.262.8935<br>
+                        aaron.perkel@gmail.com<br>
+                        Burlington VT | Macon GA
+                    </p>
+                    <p>
+                        <a href="https://github.com/aaronperkel"
+                        target="_blank"><img src="img/github-mark.png" alt="GitHub"></a>
+                        <a href="https://linkedin.com/aaronperkel"
+                        target="_blank"><img src="img/LI-In-Bug.png" alt="LinkedIn"></a>
+                    </p>
+
+                    <h3>Projects</h3>
+                    <hr>
+
+                    <p><a href="https://github.com/owncook/Blob-Kart" target="_blank">Blob Kart</a> - Spring 2024</p>
+                    <p class="grey">C++ Video Game Project inspired by Mario Kart</p>
+
+                    <p><a href="https://github.com/aaronperkel/DormKit" target="_blank">DormKit</a> - Fall 2023</p>
+                    <p class="grey">
+                        Prototype "smart dorm" product that allows a user to install 
+                        hardware in their dorm to control certain electronics from 
+                        anywhere on a college campus.
+                    </p>
+
+                    <p><a href="https://github.com/aaronperkel/Utility-Manager" target="_blank">Utility Manager</a> - Summer 2024</p>
+                    <p class="grey">
+                        A web app to help track my two roommates' 
+                        and my utility bills. Built using HTML, CSS, 
+                        PHP, SQL, and Python.
+                    </p>
+
+                    <p>“Helping Hand” - Fall 2022</p>
+                    <p class="grey">
+                        A prosthetic device to help people suffering from neuropathy 
+                        to detect high temperatures. Created a custom PCB using KiCad.
+                    </p>
+
+                    <h3>Awards</h3>
+                    <hr>
+
+                    <div class="awardsGrid">
+                        <div class="columnsOne">
+                            <p class="bold">
+                                10.23<br>‎
+                            </p>
+
+                            <p class="bold">
+                                05.21
+                            </p>
+                        </div>
+                        <div class="columnTwo">
+                            <p>
+                                Golden Key Honor 
+                                Society
+                            </p>
+                            <p>
+                                Excellence in 
+                                Technology
+                            </p>
+                        </div>
+                    </div>
+                    
+                </div>
+                <div class="columnTwo">
+                    <h3>Education</h3>
+                    <hr>
+                    <div class="educationGrid">
+                        <div class="columnOne">
+                            <p class="bold">08.22-PRESENT</p>
+                            <p class="grey ital">Graduation 05.25</p>
+                        </div>
+                        <div class="columnTwo">
+                            <p>University of Vermont - Burlington VT</p>
+                            <p class="grey ital">
+                                Bachelor of Science in Computer Science<br>
+                                Mathematics Minor
+                            </p>  
+                        </div>
+                    </div>
+                    
+                    <h3>Relavant Courses</h3>
+                    <hr>
+                    <div class="courseGrid">
+                        <div class="columnOne">
+                            <ul>
+                                <li>Calculus III</li>
+                                <li>Differential Equations</li>
+                                <li>Intermediate Programming</li>
+                                <li>Advanced Programming</li>
+                                <li>Applied Probability</li>
+                            </ul>
+                        </div>
+                        <div class="columnTwo">
+                            <ul>
+                                <li>Linear Algebra</li>
+                                <li>Computer Organization</li>
+                                <li>Computability and Complexity</li>
+                                <li>Data Structures & Algorithms</li>
+                                <li>Web Site Development</li>
+                            </ul>
+                        </div>
+                    </div>
+
+                    <h3>Skills and Interests</h3>
+                    <hr>
+                    <div class="skillsGrid">
+                        <div class="columnOne">
+                            <ul>
+                                <li>Python</li>
+                                <li>Java</li>
+                                <li>C++</li>
+                                <li>HTML / PHP / SQL</li>
+                            </ul>
+                        </div>
+                        <div class="columnTwo">
+                            <ul>
+                                <li>KiCad / PCB Design</li>
+                                <li>Fusion 360</li>
+                                <li>Video Editing</li>
+                            </ul>
+                        </div>
+                    </div>
+
+                    <h3>Work Experience</h3>
+                    <hr>
+                    <div class="workGrid">
+                        <div class="columnOne">
+                            <p class="bold">
+                                01.24-PRESENT<br>‎ 
+                            </p>
+                            <p>‎ <br>‎ <br>‎ <br>‎ <br></p>
+                            <p class="bold">
+                                11.23-PRESENT<br>‎
+                            </p> 
+                            <p>‎ <br>‎ <br>‎ <br></p>
+                            <p class="bold">   
+                                06.21-08.23
+                            </p>   
+                        </div>
+                        <div class="columnTwo">
+                            <p>
+                                Computer Science Teaching Assistant<br>
+                                UVM CS - Burlington VT
+                            </p>
+                            <p class="grey ital">
+                                Provide one on one help to over 100+ Advanced 
+                                Programming, Intermediate Programming, and 
+                                Data Structures & Algorithms students. Grade 
+                                weekly assignments.
+                            </p>
+
+                            <p>
+                                ETS Student Technician<br>
+                                UVM Tech Team - Burlington VT
+                            </p>
+                            <p class="grey ital">
+                                The Tech Team serves as the main entry point 
+                                for all IT support issues at the University of 
+                                Vermont.
+                            </p>
+
+                            <p>
+                                Mover<br>
+                                Previews Interiors and Antiques - Macon GA
+                            </p>
+                            <p class="grey ital">
+                                Drove moving van and delivered clients 
+                                furniture to their homes. Moved large items 
+                                around workshop.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <hr>
+            <p><a href="aaron-perkel-resume.pdf" download>Download</a></p>
+        </main>
+        <?php include 'footer.php'; ?>   
