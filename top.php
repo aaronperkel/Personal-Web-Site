654bf02044f685300939bfadcca6db7a0aab53ea
aaronperkel <aaron.perkel27@gmail.com>
2024-07-29 23:54:21 -0400

    enhancing SEO

--- top.php
+++ top.php
@@ -8,8 +8,12 @@ $pathParts = pathinfo($phpSelf);
     <head>
         <meta charset="utf-8">
         <title>Aaron Perkel</title>
-        <meta name="author" content="Aaron Perkel">
-        <meta name="description" content="The personal website of Aaron Perkel">
+        <meta name="author" content="Aaron Perkel - Personal Site">
+        <meta name="description" content="
+            Welcome to the personal site of Aaron Perkel. 
+            Find projects, information about me, as well as my resume. 
+            Moving to aaronperkel.com by 01/01/26.
+        ">
 
         <meta name="viewport" content="width=device-width, 
         initial-scale=1.0">
