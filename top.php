ee58455a45a83ea16d8f51fad1a428291600afc0
aaronperkel <aaron.perkel27@gmail.com>
2024-08-02 00:05:59 -0400

    trying to edit meta description

--- top.php
+++ top.php
@@ -9,11 +9,7 @@ $pathParts = pathinfo($phpSelf);
         <meta charset="utf-8">
         <title>Aaron Perkel - Personal Site</title>
         <meta name="author" content="Aaron Perkel">
-        <meta name="description" content="
-            Welcome to the personal site of Aaron Perkel. 
-            Find projects, information about me, as well as my resume. 
-            Moving to aaronperkel.com by 01/01/26.
-        ">
+        <meta name="description" content="Welcome to the personal site of Aaron Perkel. Find projects, information about me, as well as my resume. Moving to aaronperkel.com by 01/01/26.">
 
         <meta name="viewport" content="width=device-width, 
         initial-scale=1.0">
