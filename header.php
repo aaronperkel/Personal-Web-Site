38a12a96bcd4e03dee20fccdbb25374e8522c55f
aaronperkel <aaron.perkel27@gmail.com>
2024-10-22 14:07:36 -0400

    added headshot to header

--- header.php
+++ header.php
@@ -1,3 +1,7 @@
         <header>
-            <h1>Aaron Perkel</h1>
+            <div class="header">
+                <!-- <span style="height: 85px; width: 85px; background-color: #000; border-radius: 50%; display:inline-block; margin-right: 20px;"></span> -->
+                <img src="img/headshot.jpg" alt="headshot">
+                <h1>Aaron Perkel</h1>
+            </div>
         </header>
