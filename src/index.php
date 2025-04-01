fb793bd19059cbae6478b6f3d110cc5e903ad62d
aaronperkel <aaron.perkel27@gmail.com>
2025-03-31 22:08:45 -0400

    changed to font awesome

--- src/index.php
+++ src/index.php
@@ -22,31 +22,31 @@
         <h2>Projects</h2>
         <div class="projectsBox">
             <div class="imgWrapper" onclick="openPopup('content9')" data-overlay="UVM Sublets">
-                <img src="img/sublet.png" alt="UVM Sublets">
+                <img src="./public/img/sublet.png" alt="UVM Sublets">
             </div>
             <div class="imgWrapper" onclick="openPopup('content3')" data-overlay="Utility Manager">
-                <img src="img/utility.gif" alt="Utility Manager">
+                <img src="./public/img/utility.gif" alt="Utility Manager">
             </div>
             <div class="imgWrapper" onclick="openPopup('content1')" data-overlay="Blob Kart">
-                <img src="img/img.gif" alt="Blob Kart">
+                <img src="./public/img/img.gif" alt="Blob Kart">
             </div>
             <div class="imgWrapper" onclick="openPopup('content2')" data-overlay="Web Site Dev. Final">
-                <img src="img/cs1080final.png" alt="Web Site Dev. Final">
+                <img src="./public/img/cs1080final.png" alt="Web Site Dev. Final">
             </div>
             <div class="imgWrapper" onclick="openPopup('content4')" data-overlay="DormKit">
-                <img src="img/dormkit.JPG" alt="DormKit">
+                <img src="./public/img/dormkit.JPG" alt="DormKit">
             </div>
             <div class="imgWrapper" onclick="openPopup('content5')" data-overlay="Custom PCB">
-                <img src="img/pcb.png" alt="Custom PCB">
+                <img src="./public/img/pcb.png" alt="Custom PCB">
             </div>
             <div class="imgWrapper" onclick="openPopup('content6')" data-overlay="Lights Out">
-                <img src="img/lightsout.gif" alt="Lights Out">
+                <img src="./public/img/lightsout.gif" alt="Lights Out">
             </div>
             <div class="imgWrapper" onclick="openPopup('content7')" data-overlay="CodeBuilder">
-                <img src="img/combine.png" alt="CodeBuilder">
+                <img src="./public/img/combine.png" alt="CodeBuilder">
             </div>
             <div class="imgWrapper" onclick="openPopup('content8')" data-overlay="Room Status Sign">
-                <img src="img/sign.png" alt="Room Status Sign">
+                <img src="./public/img/sign.png" alt="Room Status Sign">
             </div>
         </div>
         <div id="popup" class="popup" onclick="closePopup(event)">
@@ -74,21 +74,21 @@
 
     <section>
         <h2>Socials</h2>
-        <div class="socials noUnderline">
+        <div class="socials">
             <a href="https://github.com/aaronperkel" target="_blank">
-                <img src="img/github-mark.png" alt="GitHub" width="45">
+                <i class="fa-brands fa-github fa-3x" style="color: rgb(37, 41, 46);"></i>
             </a>
             <a href="https://linkedin.com/in/aaronperkel" target="_blank">
-                <img src="img/LI-In-Bug.png" alt="LinkedIn" width="50">
+                <i class="fa-brands fa-linkedin fa-3x" style="color: rgb(57, 102, 173);"></i>
             </a>
             <a href="https://youtube.com/@aaronperkel" target="_blank">
-                <img src="img/youtube.png" alt="YouTube" width="45">
+                <i class="fa-brands fa-youtube fa-3x" style="color: rgb(218, 57, 50);"></i>
             </a>
             <a href="https://facebook.com/aaroncperkel" target="_blank">
-                <img src="img/facebook.svg" alt="Facebook" width="45">
+                <i class="fa-brands fa-facebook fa-3x" style="color: rgb(57, 117, 234);"></i>
             </a>
             <a href="https://instagram.com/aaronperkel" target="_blank">
-                <img src="img/instagram.svg" alt="Instagram" width="45">
+                <i class="fa-brands fa-instagram fa-3x"></i>
             </a>
         </div>
     </section>
